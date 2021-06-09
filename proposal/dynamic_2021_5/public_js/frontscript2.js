a23c07116b7076ec165fd304e101964e8a354ab8
chengshichao <thatwhyme@gmail.com>
2021-06-09 23:22:25 +0800

    for a test thatwhyme

---
+++ proposal/dynamic_2021_5/public_js/frontscript2.js
@@ -0,0 +1,11 @@
+// var slider = document.getElementById("myRange");
+// var output = document.getElementById("Proportion");
+// output.innerHTML = slider.value + "%";
+// slider.oninput = function() {
+    
+//     output.innerHTML = this.value + "%";
+//     //here should change the text for red player
+//     Max_Choice_Num = Math.floor(Math.pow(2,depth-1)*this.value/100)
+//     filTxt()
+//     // console.log(output.innerHTML)
+// }
